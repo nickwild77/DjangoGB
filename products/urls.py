2f10f9eb739ed7a3b2bae20bc4d4647d667c4f44
nickwild77 <knezhevich77@gmail.com>
2021-09-05 19:47:01 +0300

    Доавленны urls в продуктах

--- products/urls.py
+++ products/urls.py
@@ -13,20 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
-from products.views import index, products
+app_name = 'products'
+
+from products.views import products
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', include('products.urls', namespace='products'))
-    #path('products/', products, name='products'),
-
+    path('', products, name='index'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
