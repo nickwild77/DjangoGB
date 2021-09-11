8a64b081b7fe6916b591e14dc5522c88bc441d14
nickwild77 <knezhevich77@gmail.com>
2021-09-11 21:40:27 +0300

    обновлены templates

--- users/views.py
+++ users/views.py
@@ -1,14 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+from .forms import UserLoginForm
 
 
 # Create your views here.
 
 def login(request):
-    context = {
-        'title': 'Geekshop - Авторизация'
-    }
 
-    return render(request, 'users/login.html', context)
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
+        context = {
+            'title': 'Geekshop - Авторизация',
+            'form': form
+        }
+
+        return render(request, 'users/login.html', context)
 
 
 def register(request):
