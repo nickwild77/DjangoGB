30e45c780d0b7a539c7885744bcc77413792e439
nickwild77 <knezhevich77@gmail.com>
2021-09-28 00:42:26 +0300

    added new admin panel

--- baskets/admin.py
+++ baskets/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from baskets.models import Basket
 
-admin.site.register(Basket)
+
+# admin.site.register(Basket)
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp', 'update_timestamp')
+    readonly_fields = ('created_timestamp', 'update_timestamp')
+    extra = 0
