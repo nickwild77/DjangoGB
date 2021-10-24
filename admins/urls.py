7cb1c511737770e4eb335464e473681353f5de42
nickwild77 <knezhevich77@gmail.com>
2021-10-24 19:19:49 +0300

    added CBV 2

--- admins/urls.py
+++ admins/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 
 app_name = 'admins'
 
-from .views import index, admin_users_delete, admin_users_update, admin_users_create, admin_categories, UserListView
+from .views import index, admin_users_delete, admin_users_update, admin_categories, UserListView, UserCreateView
 
 urlpatterns = [
     path('', index, name='index'),
     path('users/', UserListView.as_view(), name='admin_users'),
-    path('user-create/', admin_users_create, name='admin_users_create'),
+    path('user-create/', UserCreateView.as_view(), name='admin_users_create'),
     path('user-update/<int:id>', admin_users_update, name='admin_users_update'),
     path('user-delete/<int:id>', admin_users_delete, name='admin_users_delete'),
     path('categories/', admin_categories, name='admin_categories'),
--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
 from django.utils.decorators import method_decorator
 from django.views.generic.list import ListView
@@ -36,22 +36,37 @@ class UserListView(ListView):
 #     }
 #     return render(request, 'admins/admin-users-read.html', context)
 
+class UserCreateView(CreateView):
+    model = User
+    template_name = 'admins/admin-users-create.html'
+    form_class = UserAdminRegisterForm
+    success_url = reverse_lazy('admins:admin_users')
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users_create(request):
-    if request.method == 'POST':
-        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    else:
-        form = UserAdminRegisterForm()
-    context = {
-        'title': 'GeekShop - Aдмин | Регистрация',
-        'form': form
-    }
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(UserCreateView, self).get_context_data(**kwargs)
+        context['title'] = 'Aдминка | Регистрация'
+        return context
 
-    return render(request, 'admins/admin-users-create.html', context)
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
+
+
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_users_create(request):
+#     if request.method == 'POST':
+#         form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('admins:admin_users'))
+#     else:
+#         form = UserAdminRegisterForm()
+#     context = {
+#         'title': 'GeekShop - Aдмин | Регистрация',
+#         'form': form
+#     }
+#
+#     return render(request, 'admins/admin-users-create.html', context)
 
 
 @user_passes_test(lambda u: u.is_superuser)
