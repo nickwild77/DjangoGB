d921f84a3d38f0de4c5845e4663aa06f88874324
nickwild77 <knezhevich77@gmail.com>
2021-10-24 19:01:34 +0300

    added CBV

--- admins/views.py
+++ admins/views.py
@@ -2,21 +2,39 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import user_passes_test
+from django.utils.decorators import method_decorator
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
 from users.models import User
+from products.models import ProductsCategory
 
 
 def index(request):
     return render(request, 'admins/admin.html')
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users(request):
-    context = {
-        'users': User.objects.all()
-    }
-    return render(request, 'admins/admin-users-read.html', context)
+class UserListView(ListView):
+    model = User
+    template_name = 'admins/admin-users-read.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(UserListView, self).get_context_data(**kwargs)
+        context['title'] = 'Админка | Пользователи'
+        return context
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
+
+
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_users(request):
+#     context = {
+#         'users': User.objects.all()
+#     }
+#     return render(request, 'admins/admin-users-read.html', context)
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -60,3 +78,11 @@ def admin_users_delete(request, id):
     user.is_active = False
     user.save()
     return HttpResponseRedirect(reverse('admins:admin_users'))
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_categories(request):
+    context = {
+        'categories': ProductsCategory.objects.all()
+    }
+    return render(request, 'admins/admin-categories-read.html', context)
