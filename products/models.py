be0aff0b108485f56887de897bbcef1412ab9a2f
nickwild77 <knezhevich77@gmail.com>
2021-11-06 17:46:58 +0300

    added CBV users, categories, products

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
@@ -6,6 +7,14 @@ from django.db import models
 class ProductsCategory(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this category should be treated as active. '
+            'Unselect this instead of category accounts.'
+        ),
+    )
 
     def __str__(self):
         return self.name
@@ -18,6 +27,14 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductsCategory, on_delete=models.CASCADE)
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this product should be treated as active. '
+            'Unselect this instead of product accounts.'
+        ),
+    )
 
     def __str__(self):
         return f'{self.name} | {self.category}'
