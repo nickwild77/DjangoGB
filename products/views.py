f8aee257afe962fd4e9657ef1dbde4520698de92
nickwild77 <knezhevich77@gmail.com>
2021-12-05 22:48:27 +0300

    refactor, debag tools

--- products/views.py
+++ products/views.py
@@ -1,14 +1,59 @@
+from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from django.core.cache import cache
+
 import os
 import json
+
+from django.views.decorators.cache import cache_page, never_cache
+from django.views.generic import DetailView
 
 from products.models import Product, ProductsCategory
 
 MODULE_DIR = os.path.dirname(__file__)
+
+
+def get_links_category():
+    if settings.LOW_CACHE:
+        key = 'links_category'
+        links_category = cache.get(key)
+
+        if links_category is None:
+            links_category = ProductsCategory.objects.filter(is_active=True)
+            cache.set(key, links_category)
+        return links_category
+    else:
+        return ProductsCategory.objects.filter(is_active=True)
+
+
+def get_product(pk):
+    if settings.LOW_CACHE:
+        key = f'product{pk}'
+        product = cache.get(key)
+
+        if product is None:
+            product = get_object_or_404(Product, pk=pk)
+            cache.set(key, product)
+        return product
+    else:
+        return get_object_or_404(Product, pk=pk)
+
+
+def get_links_product():
+    if settings.LOW_CACHE:
+        key = 'links_product'
+        links_product = cache.get(key)
+
+        if links_product is None:
+            links_product = Product.objects.filter(is_active=True).select_related()
+            cache.set(key, links_product)
+        return links_product
+    else:
+        return Product.objects.filter(is_active=True).select_related()
 
 
 def index(request):
@@ -16,8 +61,10 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
+@never_cache
 def products(request, id=None, page=1):
-    products = Product.objects.filter(category_id=id) if id != None else Product.objects.all()
+    products = Product.objects.filter(category_id=id).select_related('category') \
+        if id != None else Product.objects.all().select_related('category')
     paginator = Paginator(products, per_page=3)
     try:
         products_paginator = paginator.page(page)
@@ -44,3 +91,20 @@ def get_price(request, product_id):
         product = get_object_or_404(Product, id=product_id, is_active=True)
         price = product.price
         return JsonResponse({'status': True, 'price': price})
+
+
+class ProductDetail(DetailView):
+    """
+    Контроллер вывода информации о продукте
+    """
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
+
+    def get_context_data(self, category_id=None, *args, **kwargs):
+        """Добавляем список категорий для вывода сайдбара с категориями на странице каталога"""
+        context = super().get_context_data()
+
+        context['product'] = get_product(self.kwargs.get('pk'))
+        context['categories'] = ProductsCategory.objects.all()
+        return context
--- static/js/orders_scripts.js
+++ static/js/orders_scripts.js
@@ -78,7 +78,7 @@ window.onload = function () {
     }
 
 
-    $('.order_form select').change(function () {
+    $('.order_form').on('change', 'select', function () {
 
         let target = event.target;
         orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
@@ -104,8 +104,21 @@ window.onload = function () {
             });
         }
 
-
     });
 
+    $('.basket_list').on('click', 'input[type="number"]', function () {
+
+        let t_href = event.target;
+        console.log(t_href.name);
+        console.log(t_href.value);
+
+        $.ajax({
+            url: '/baskets/edit/' + t_href.name + '/' + t_href.value + '/',
+            success: function (data) {
+                $('.basket_list').html(data.result)
+            },
+        });
+        Event.preventDefault();
+    })
 
 }
